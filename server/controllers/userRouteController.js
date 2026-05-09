2407c138a4840b6ff5193113573acf8d60acd806
Pranav Deshmukh <pranavdeshmukh2512@gmail.com>
2026-05-09 22:25:15 +0530

    Implement user login and logout functionality in userRouteController

--- server/controllers/userRouteController.js
+++ server/controllers/userRouteController.js
@@ -43,3 +43,49 @@ export const userRegister = async (req, res) => {
         console.log(error);
     }
 }
+
+export const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email })
+        if (!user) return res.status(500).send({ success: false, message: "Email Dosen't Exist Register" })
+        const comparePasss = bcryptjs.compareSync(password, user.password || "");
+        if (!comparePasss) return res.status(500).send({ success: false, message: "Email Or Password dosen't Matching" })
+        
+        jwtToken(user._id, res);
+
+        res.status(200).send({
+            _id: user._id,
+            fullname: user.fullname,
+            username: user.username,
+            profilepic: user.profilepic,
+            email:user.email,
+            message: "Succesfully LogIn"
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: error
+        })
+        console.log(error);
+    }
+}
+
+
+export const userLogOut=async(req,res)=>{
+    
+    try {
+        res.cookie("jwt",'',{
+            maxAge:0
+        })
+        res.status(200).send({success:true ,message:"User LogOut"})
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: error
+        })
+        console.log(error);
+    }
+}
--- server/route/authUser.js
+++ server/route/authUser.js
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.post('/register',userRegister);
 
+router.post('/login',userLogin)
+
+router.post('/logout',userLogOut)
+
 export default router
